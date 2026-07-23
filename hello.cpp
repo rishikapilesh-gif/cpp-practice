031b5904d4fa55d2b8fb6c7584d7a1614d334216
Rishi <rishikapilesh@example.com>
2026-07-23 04:25:00 +0000

    My first c++ program

--- hello.cpp
+++ hello.cpp
@@ -2,6 +2,6 @@
 using namespace std;
 int main() 
 {
-    cout << "Hello Rishi! Your first C++ program hunai jk"<< endl;
+    cout << "Hello Rishi! Your first C++ program has been successful!"<< endl;
     return 0;
 }
