8aaa975bf0aabebbbe94a0f745e0b02ebbaf2924
Rishi <rishikapilesh@example.com>
2026-07-23 05:18:31 +0000

    concatenation using append

---
+++ BASICS /Added_concatenation_using_append.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+
+
+    string str1 = "Hello, ";
+    string str2 = "Iam a coder";
+    cout<<str1.append(str2)<<endl;
+    return 0;
+}
