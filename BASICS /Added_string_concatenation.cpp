46a66ec22bc1dcc8abfd4715d7c547f2b623c1fb
Rishi <rishikapilesh@example.com>
2026-07-23 05:06:48 +0000

    My basic programming stuffs

---
+++ BASICS /Added_string_concatenation.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+
+    string str1 = "Hello, ";
+    string str2 = "Iam a coder";
+    string str3 = str1 + str2;
+    cout << str3 << endl;
+    return 0;
+}
